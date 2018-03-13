4ae15eb3be9ad2190dbbc87ee5d95749f18c1aae
UladzislauValchkou <35953713+UladzislauValchkou@users.noreply.github.com>
2018-03-13 11:22:16 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -2,5 +2,5 @@
 #pylint:disable=invalid-name
 """ task 4 """
 N = int(input("Enter integer number: "))
-for a in range(N)[1:]:
+for a in range(1, N+1):
     print("{:>{width}}{:>{width}}{:>{width}}{:>{width}}".format(int(a), oct(a)[2:], hex(a)[2:], bin(a)[2:], width=len(str(bin(N)[2:])) + 1))
